3003b96c1efc85e68aaa82b16a93e84ce76238bf
Michael Macuha <b2028madmacuha@pshs.edu.ph>
2025-04-07 12:58:14 +0800

    sirroy

---
+++ public/script.js
@@ -0,0 +1,20 @@
+
+
+  // PLEASE STUDY THE CODES BELOW, BUT DO NOT CHANGE ANYTHING 
+
+  // this function will create the needed input fields and corresponding Going checkboxes for the number of expected guests
+
+  
+  // A quick data dump on Output div to show users input on the browser.
+  function printFormData() {
+    const form = document.getElementById('input_form');
+    const formData = new FormData(form);
+    const outputDiv = document.getElementById('output');
+    outputDiv.innerHTML = '<h2> Output <h2>';
+    for (let [key, value] of formData.entries()){
+      outputDiv.innerHTML += `${key}: ${value} <br>`;
+    }
+    const myData = Object.fromEntries(formData.entries());
+    console.log(myData)
+    console.log(formData.entries())
+  }
---
+++ server.js
@@ -0,0 +1,41 @@
+// Loads the express module
+const express = require("express");
+const hbs = require("hbs");
+
+const bodyParser = require("body-parser");
+
+const path = require("path");
+
+//Creates our express server
+const app = express();
+const port = 3000;
+
+//Serves static files (we need it to import a css file)
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "hbs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Sets a basic route
+
+// Render the initial page with the number input form
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// Create express route binder for draw.hbs and get the data from the url as parameters
+// that came from index.hbs
+
+  app.post("/happy", (req, res) => {
+    const { name } = req.body;
+    const { gender } = req.body;
+    const { number } = req.body;
+    const { checkbox } = req.body;
+    const { invited } = req.body;
+    res.render("happy", { name, gender, number, checkbox, invited });
+  });
+  app.get("/happy", (req, res) => {
+    res.render('happy');
+  });
+
+//Makes the app listen to port 3000
+app.listen(port, () => console.log(`App listening to port ${port}`));
