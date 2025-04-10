446065ef321715d728aef9dcca607788b467a8db
Michael Macuha <b2028madmacuha@pshs.edu.ph>
2025-04-10 08:38:16 +0800

    me

--- public/script.js
+++ public/script.js
@@ -1,9 +1,20 @@
+function solve(){
+// access the form defined in index and create a form data object using FormData()
+  const form = document.getElementById('input_form');
+  const formData = new FormData(form);
+   // get the name of the celebrant
+  console.log(formData.get("name"));
+  const name = formData.get('name');
+
+  // get and checks the gender
+  console.log(formData.get('gender'));
+  let gender = formData.get('gender');
+}
 
 
   // PLEASE STUDY THE CODES BELOW, BUT DO NOT CHANGE ANYTHING 
 
   // this function will create the needed input fields and corresponding Going checkboxes for the number of expected guests
-
   
   // A quick data dump on Output div to show users input on the browser.
   function printFormData() {
--- server.js
+++ server.js
@@ -26,12 +26,63 @@ app.get("/", (req, res) => {
 // that came from index.hbs
 
   app.post("/happy", (req, res) => {
+
     const { name } = req.body;
     const { gender } = req.body;
     const { number } = req.body;
-    const { checkbox } = req.body;
-    const { invited } = req.body;
-    res.render("happy", { name, gender, number, checkbox, invited });
+    // const { checkbox } = req.body;
+    // const { invited } = req.body;
+    const { checkbox1 } = req.body;
+    const { name1 } = req.body;
+    const { checkbox2 } = req.body;
+    const { name2 } = req.body;
+
+    console.log(name, gender, number, checkbox1, name1, name2, checkbox2);
+
+  
+    let arrayName = new Array();
+// to get the invited guests inside of arrayName
+for (let i = 1; i <= 16; i++) {
+  const checkbox = req.body[`checkbox${i}`];
+
+  if (!req.body[`name${i}`];) {
+    break;
+  } 
+  if (checkbox !== undefined) {
+    arrayName.push(req.body[`name${i}`];);
+  }
+}
+
+
+    let songTotal = "Happy birthday to you. Happy birthday to you. Happy birthday dear _. Happy birthday to you! For __ a jolly good fellow. For __ a jolly good fellow. For __ a jolly good fellow, which nobody can deny!"
+    let song = songTotal.split(' ');
+    let happy = ' ';
+
+    console.log(song);
+// to change the blanks to their respective names and genders
+    for(key in song) {
+
+      if(song[key] == '_.') {
+        song[key] = name;
+      } else if (song[key] == '__') {
+        if(gender == "male") {
+          song[key] = "he's";
+        } else {
+          song[key] = "she's";
+        }
+      }
+    }
+//making the output to be sent in the happy.hbs file
+    let output = '';
+    let nameCounter = 0;
+    for(key in song) {
+      nameCounter == arrayName.length ? nameCounter = 0 : null;
+      output += arrayName[nameCounter] + ": " + song[key] + "<br>";
+      nameCounter++;
+    }
+    console.log(output);
+    res.render("happy", { name, gender, number, checkbox1, name1, checkbox2, name2, output });
+
   });
   app.get("/happy", (req, res) => {
     res.render('happy');
